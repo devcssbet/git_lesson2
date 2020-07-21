81ac9d87e967a7dc3d3d3443be008db46367f437
Some name <Some_name@mymail.com>
2020-07-21 14:21:53 +0200

    added a second function for calculating square

--- src/my_square.py
+++ src/my_square.py
@@ -1,7 +1,11 @@
 def my_square(y):
 	"""takes a value and returns the squared value.
-	uses the ** operator
+	uses the * operator
 	"""
 	return(y ** 2)
 
+def my_square2(x):
+	return(x * x)
+
 print (my_square(42))
+print (my_square2(42))
